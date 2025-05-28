717332776cd1b6750f069ade01938f1ce26ea6bb
ls <2023465lim@aupp.edu.kh>
2025-05-28 23:14:56 +0700

    next is to create api

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,7 +10,7 @@ const TabIcon = ({ focused, title, icon }: { focused: boolean; title: string; ic
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image
           source={icon}
@@ -35,8 +35,29 @@ const TabIcon = ({ focused, title, icon }: { focused: boolean; title: string; ic
 const _Layout = () => {
   return (
 
-    <Tabs>
-        <Tabs.Screen 
+    <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarItemStyle: {
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          },
+          tabBarStyle: {
+            backgroundColor: "#0f0D23",
+            borderRadius: 50,
+            marginHorizontal: 20,
+            marginBottom: 36,
+            height: 52,
+            position: "absolute",
+            overflow: "hidden",
+            borderWidth: 1,
+            borderColor: "#0f0d23"
+          },
+        }}
+      >
+        <Tabs.Screen
             name="index"
             options={{
                 title: "Home",
